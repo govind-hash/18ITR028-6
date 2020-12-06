f5ecd5b0e721f47fb1068a7231b14c866155c9cd
govind-hash <75358261+govind-hash@users.noreply.github.com>
2020-12-06 23:45:12 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,31 @@
+document.querySelector('#cancelbtn').addEventListener('click',() =>{
+    document.querySelector('#name').value='';
+  document.querySelector('#password').value='';
+})
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/static/index.html');
+});
+
+
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + '/static/login.html');
+});
+
+app.post('/login', (req, res) => {
+  
+  let username = req.body.username;
+  let password = req.body.password;
+  res.send(`Welcome ${username} 
+  Password: ${password}`);
+});
+
+const port = 3000 
+app.listen(port, () => console.log(`This app is listening on port ${port}`));
